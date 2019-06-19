d9413a2fc78f8f742890b2661379868ea8ecf76a
efrainmejiasc <efrainmejiasc@hotmail.com>
2019-06-19 15:51:53 -0400

    INIT

---
+++ JobScheduler/JobScheduler/EngineStf.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobScheduler
+{
+    public class EngineStf
+    {
+        public async Task<List<KactusIntegration.Empleado>> EmpleadoKactusAsync()
+        {
+            string resultado = string.Empty;
+            string userWcf = "intagata";
+            string passwordWcf = "KqQ*O3XI*y";
+            KactusIntegration.KWsGhst2Client wsGhst2Client = new KactusIntegration.KWsGhst2Client();
+            wsGhst2Client.Endpoint.Binding.SendTimeout = new TimeSpan(0, 5, 0);
+            List<KactusIntegration.Empleado> KactusEmpleado = new List<KactusIntegration.Empleado>();
+            var response = await wsGhst2Client.ConsultarEmpleadosAsync(499, DateTime.Now.AddDays(-20), userWcf, passwordWcf);
+            resultado = Newtonsoft.Json.JsonConvert.SerializeObject(response);
+            KactusEmpleado = response.ToList();
+            EngineDb Metodo = new EngineDb();
+            Metodo.InsertKactusEmpleados("Sp_InsertKactusEmpleado", KactusEmpleado);
+            return KactusEmpleado;
+        }
+    }
+}
---
+++ JobScheduler/JobScheduler/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Spi;
+
+namespace JobScheduler
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add Quartz services
+            services.AddSingleton<IJobFactory, SingletonJobFactory>();
+            services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
+
+            // Add our job
+            services.AddSingleton<ChronJob>();
+            services.AddSingleton(new JobSchedule(
+                jobType: typeof(ChronJob),
+                cronExpression: "0 08 15 ? * *"));
+
+            services.AddHostedService<QuartzHostedService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
